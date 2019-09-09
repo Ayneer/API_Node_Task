1920681e491cac7b1b6c81530ad39562c4f6a5df
Ayneer Gonzalez <ayneer12@gmail.com>
2019-09-08 23:40:20 -0500

    some changes light

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const routes = require('./src/routes');
 require('./src/data');
@@ -12,13 +12,7 @@ app.set('PORT', process.env.PORT || 3500);
 app.use(bodyParse.urlencoded({extended:false}));
 app.use(express.json());
 // SOLO EN MODO DEV, EN PRODUCCIÓN BORRAR ESTO!!
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+app.use(cors());
 app.use('/', routes);
 
 db.once('open', ()=>{
